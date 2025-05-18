173aeebc8dd1038fea22467c008a69d42f8c6afc
Dmytro <d.salyakin@gmail.com>
2025-05-18 18:06:34 +0300

    Add other CRUD operations for Lesson-6

---
+++ lesson-6/01-crud/src/db.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+function initDatabaseConnection() {
+  return mongoose.connect(getEnvVar('MONGODB_URI'));
+}
+
+export { initDatabaseConnection };
